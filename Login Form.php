29d886ba87acf31c092d7336c0f9b1fed1c19476
saaif44 <41290426+saaif44@users.noreply.github.com>
2023-09-24 09:51:35 +0600

    Add files via upload

---
+++ Login Form.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    
+
+<form>
+    <center>
+<h3>Login Page</h3>
+  <label for="username">Uname:</label>  <input type="text" ><hr/>
+  <label for="pass">Password</label>    <input type="password" ><hr>
+  <input type="submit" value="Submit">
+  </center>
+</form>
+
+<br>
+<br>
+
+<center><h3>Course</h3>
+<table  border="1">
+
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Course</th>
+</tr>
+
+<tr>
+    <td>17</td>
+    <td>Ab</td>
+    <td rowspan="2">Web</td>
+</tr>
+<tr>
+    <td>19</td>
+    <td rowspan="2">Cd</td>
+    
+</tr>
+<tr>
+    <td>20</td>
+    <td>CN</td>
+</tr>
+
+</table>
+</center>
+
+
+</body>
+</html>
